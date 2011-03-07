a799dcd43e0f0bf919f7e6de1f294a874e4c9e6c
remi.lacroix <remi.lacroix@cea4b8a3-3092-e560-fe6c-55f17fc48ad2>
2011-03-07 14:44:36 +0000

    La partie analyse lexicale et syntaxique.
    La grammaire n'est pas encore totalement complète et il reste des conflits (sans compter le fait que ça ne fait probablement pas exactement ce qu'on voudrait ...).

---
+++ trunk/main.h
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+
+/* deux macros pratiques, utilisées dans les allocations */
+#define NEW(howmany, type) (type *) calloc((unsigned) howmany, sizeof(type))
+#define nil(type) (type *) 0
+
+
+/* Etiquettes pour les noeuds de l'arbre de syntaxe abstraite representant une
+ * expression.
+ * Les opérateurs arithmétiques sont directement utilisés aussi comme
+ * etiquettes.
+ */
+typedef enum { 
+  Id, 
+  Cste, 
+  EQ, NEQ, GT, GE, LT, LE, /* les differents operateurs de comparaison */
+  ITE,                     /* le if-then-else */
+  NOP, 			   /* etiquette "auxiliaire */
+  Opp,			   /* l'oppose */
+  GetEti,
+  PutEti
+} etiquette;
+
+
+
+/* la structure ci-dessous permet de memoriser des listes variable/valeur
+ * (entiere). On va construire des listes de la forme { (x 5), (y, 27) } au fur
+ * et a mesure qu'on interprete les declarations dans le programme.
+ */
+typedef struct var
+{ char *nom;
+  int val;
+  struct var *suiv;
+} VAR, *PVAR;
+
+
+/* la structure d'un noeud de l'arbre: un noeud interne ou une feuille */
+typedef union {
+        char *S;	  /* feuille de type Identificateur */
+        int E;	 	  /* feuille de type constante entiere */
+        struct arbre *A;  /* noeud interne : un operateur et deux operandes */
+} NOEUD;
+
+
+/* la structure d'un noeud interne ... */
+typedef struct arbre
+{ char op;		/* une etiquette: voir l'enumeration ci-dessus */
+  NOEUD gauche, droit;  /* deux noeuds: internes ou feuilles */
+} ARBRE, *PARBRE;
+
+
+typedef union
+{ char C;		/* necessaire pour flex */
+  PVAR V;		/* les autres correspondent aux variantes utilisees */
+  PARBRE A;		/* dans les actions associees aux productions de    */
+  int E;		/* la grammaire. 				    */
+  char *S;
+} YYSTYPE;
+
+
+#define YYSTYPE YYSTYPE
