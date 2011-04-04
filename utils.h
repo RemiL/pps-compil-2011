2b28c9041d945ec5e3ed1a1e8fe5bd3749d4e30e
remi.lacroix@gmail.com <remi.lacroix@gmail.com@cea4b8a3-3092-e560-fe6c-55f17fc48ad2>
2011-04-04 08:02:11 +0000

    Centralisation des macros utiles.

---
+++ utils.h
@@ -0,0 +1,11 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#define VRAI 1
+#define FAUX 0
+
+/* deux macros pratiques, utilisées dans les allocations */
+#define NEW(howmany, type) (type *) calloc((unsigned) howmany, sizeof(type))
+#define NIL(type) (type *) 0
+
+#endif
