d8ea2a3d33c50ebff03d032f12e8a071c5a8ec6c
remi.lacroix@gmail.com <remi.lacroix@gmail.com@cea4b8a3-3092-e560-fe6c-55f17fc48ad2>
2011-04-13 09:15:35 +0000

    - Les fonctionnalités de vérification contextuelles sont maintenant réellement utilisées. - Correction : les paramètres sont maintenant pris en compte lors de la résolution d'un identifiant (utilisation d'une structure supplémentaire à voir comme une pile de déclarations de variables et de paramètres afin de gérer la portée des variables sans trop se prendre la tête). - Correction : les variables locales (déclarées au niveau d'un bloc) n'étaient jamais vérifiées. - Correction : ajout des retours à la ligne manquants dans les messages d'erreur.
    NOTE : actuellement l'exemple 1 ne passe pas la vérification syntaxique. Ceci est dû au fait que la vérification des types devrait être faite modulo héritage ce qui n'est pas encore le cas (mais ça va venir).

--- trunk/verif_contextuelles.h
+++ trunk/verif_contextuelles.h
@@ -12,8 +12,9 @@ void sont_valides_params(liste_classes_t decl, classe_t* classe, methode_t* meth
 void est_valide_methode(liste_methodes_t liste, methode_t* methode);
 void est_valide_redefinition(classe_t* classe, methode_t* methode, methode_t* methode_redefinie);
 void est_valide_param(liste_params_t liste, param_t* parametre);
-void sont_valides_arguments(liste_classes_t decl_classes, liste_vars_t decl_vars, methode_t* methode, liste_args_t arguments);
 void est_valide_attribut(classe_t* classe, char* attribut);
-classe_t* est_valide_arbre_syntaxique(liste_classes_t decl_classes, liste_vars_t decl_vars, arbre_t* arbre);
+void sont_valides_variables(liste_classes_t decl_classes, decl_vars_t* decl_vars, liste_vars_t variables);
+void sont_valides_arguments(liste_classes_t decl_classes, decl_vars_t* decl_vars, methode_t* methode, liste_args_t arguments);
+classe_t* est_valide_arbre_syntaxique(liste_classes_t decl_classes, decl_vars_t* decl_vars, arbre_t* arbre);
 
 #endif
